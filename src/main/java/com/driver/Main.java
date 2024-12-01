ec3baa41e99afaf531990cb70d31b52dd12e9f39
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-01 21:55:28 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,24 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        // Creating and testing a Vehicle
+        Vehicle vehicle = new Vehicle("Generic Vehicle");
+        System.out.println("Vehicle Name: " + vehicle.getName());
+
+        // Creating and testing a Car
+        Car car = new Car("Sports Car", 180);
+        System.out.println("Car Name: " + car.getName());
+        System.out.println("Car Speed: " + car.getSpeed() + " km/h");
+
+        // Creating and testing an F1 car
+        F1 f1 = new F1("Formula 1", 320, "Red Bull");
+        System.out.println("F1 Car Name: " + f1.getName());
+        System.out.println("F1 Car Speed: " + f1.getSpeed() + " km/h");
+        System.out.println("F1 Team: " + f1.getTeam());
+
+        // Creating and testing a Boat
+        Boat boat = new Boat("Speedboat", 15);
+        System.out.println("Boat Name: " + boat.getVehicleName());
+        System.out.println("Boat Capacity: " + boat.getVehicleCapacity() + " people");
     }
 }
