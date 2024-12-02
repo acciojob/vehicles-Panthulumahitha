15767ec50acf3b7771b17dc6cda95de2b473fe4f
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-02 20:53:28 +0530

    WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,21 +1,6 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
-    private String name;
-    private int capacity;
-
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
-    }
+public interface WaterVehicle {
+    String getVehicleName();
+    int getVehicleCapacity();
 }
